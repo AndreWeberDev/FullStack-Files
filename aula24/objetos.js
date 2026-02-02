3e741a16b4c6ff3481ce4c7877d0b1c10ded4baa
Juninnn <andrezindsytb@gmail.com>
2026-02-02 20:54:14 -0300

    upd

---
+++ aula24/objetos.js
@@ -0,0 +1,69 @@
+const monstrosFloresta = {
+
+id: 01,
+nome: "slime",
+raca:"slime",
+descricao: "Uma raça comum de slime, comumente encontrada em áreas moderadas. Eles usam seus corpos ácidos para dissolver plantas e mamíferos",
+
+id: 02,
+nome: "ladrão",
+raca: "variável",
+descricao: "Em tempos de necessidade, muitos se rebaixarão a caçar comerciantes e viajantes para sobreviver, e esses bandidos não são diferentes.",
+
+id: 03,
+nome: "espiríto da grama",
+raca: "espiríto",
+descricao: "Antigos protetores da floresta, eram amigos das raças mortais e conviviam ao lado delas, proporcionando fertilização de lavouras e terras férteis. Agora eles perderam a inteligência para a corrupção.",
+
+id: 04,
+nome: "cogumelo zumbi",
+raca: "fungo",
+descricao: "Grandes cogumelos sencientes que costumavam habitar o Pantâno Profundo antes de serem amaldiçoados. Agora eles vasculham as florestas em busca de presas fáceis para envenenar e devorar.",
+
+id: 05,
+nome: "goblin",
+raca: "goblin",
+descricao: "Goobers.",
+
+id: 05,
+nome: "Slime das Estrelas",
+raca: "Slime",
+descricao: "Slimes que assumem a forma do céu noturno, herdando um fragmento de poder celestial em homenagem a Thanasius.",
+
+};
+
+const monstrosDeserto = {
+
+id: 06,
+nome: "Elemental de Areia",
+raca: "Espirito",
+descricao: "Um Espirito formado de gerações de ventos e mortes nas dunas, eles tem a capacidade de controlar grandes quantidades de areia com facilidade, assim sendo uma das principais causas da tempestade de areia sem fim que abala o deserto desde a corrupção original",
+
+id: 07,
+nome: "Bandido do Deserto",
+raca: "variável",
+descricao: "humildes ladrões que dominaram o uso de uma antiga arte de armas da época em que o deserto florescia, agora atacam qualquer um com objetos de valor que entre no deserto.",
+
+id: 08,
+nome: "Tempestade de Areia Andarilha",
+raca: "espiríto",
+descricao: "AMassas de vento e areia em constante turbilhão, uma das principais causas dos ventos extremos e das tempestades de areia nas dunas. Alguns dizem que chegar muito perto deles pode sufocar você com o vento constante",
+
+id: 09,
+nome: "Golem de Areia",
+raca: "Golem",
+descricao: "Construções robustas de areia e rocha, usadas em todo o continente Astora para transporte e reboque devido à facilidade de domar",
+
+id: 10,
+nome: "invasor noturno",
+raca: "variavel",
+descricao: "Invasores experientes que dominaram uma antiga arte com armas da época em que o deserto florescia, agora atacam qualquer pessoa com objetos de valor que entre no deserto à noite.",
+
+
+};
+
+const monstrosMonteThul = {
+
+
+  
+}
